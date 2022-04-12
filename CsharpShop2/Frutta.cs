b8a6f33c9d776bb781c2c1ea09cd5fecfea54435
davidepetrillo <davide96petrillo@gmail.com>
2022-04-12 17:13:52 +0200

    commit 1 - definita struttura del programma

--- CsharpShop2/Frutta.cs
+++ CsharpShop2/Frutta.cs
@@ -8,14 +8,29 @@ namespace CsharpShop2
 {
     internal class Frutta : Prodotto
     {
-        
 
+        private double peso;
 
-        public Frutta(string nome, string descrizione, double prezzo, int iva) : base(nome, descrizione, prezzo, iva)
+        public Frutta(string nome, string descrizione, double peso, double prezzo, int iva) : base(nome, descrizione, prezzo, iva)
         {
            
         }
 
+        public override void StampaProdotto()
+        {
+            Console.WriteLine("----- Prodotto -------");
+            Console.WriteLine("Codice: " + codice);
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Descrizione: " + descrizione);
+            Console.WriteLine($"Peso: {prezzo}kg");
+            Console.WriteLine($"Prezzo: {prezzo} euro");
+            Console.WriteLine($"IVA: {iva}%");
+            Console.WriteLine($"Prezzo finale: {PrezzoPiuIva(prezzo, iva)} euro");
+            Identificativo(codice, nome);
+            PadLeft();
+            Console.WriteLine("------------------");
+
+        }
 
 
     }
--- CsharpShop2/Program.cs
+++ CsharpShop2/Program.cs
@@ -1,7 +1,7 @@
 ﻿using CsharpShop2;
 
 Prodotto Cereali = new Prodotto("Cereali", "Cereali a base di miele e cannella ", 1.99, 17);
-Frutta Banana = new Frutta("Banana", "Banane coltivate in Brasile e prodotte in Guatemala", 1.69, 19);
+Frutta Banana = new Frutta("Banana", "Banane coltivate in Brasile e prodotte in Guatemala", 2, 1.69, 19);
 Acqua Lete = new Acqua("Lete", "L’acqua che il tuo corpo vuole", 2.19, 18, 6, 1.5, "plastica", 6.30, "Appennino meridionale");
 Elettrodomestico Lavatrice = new Elettrodomestico("Lavatrice", "Samsung Elettrodomestici WD10T534DBW/S3", 70, 10.5, 1400, 60, 60, 85, "bianco", 679.00, 22);
 CiboInScatola Tonno = new CiboInScatola("Tonno", "Nostromo - Tonno Leggero all'Olio di Oliva con -60% di Grassi, Qualità Pinne Gialle, Senza Conservanti", 6, 60, 2.99, 8, "12/09/2023");
